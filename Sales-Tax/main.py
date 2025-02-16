cfb258a3ed6c6399ea9d9e2a94ed3672007b4adb
Jay <jaykahoun@westernwyoming.edu>
2025-02-16 04:22:54 +0000

    Ready for Grading.

--- Sales-Tax/main.py
+++ Sales-Tax/main.py
@@ -1,22 +1,39 @@
 #
-# Name
-# Date
+# Jay 
+# 02/15/2025
 # Sales Tax Programming Project
-# COSC 2409 DNT
+# COSC 1010
 #
 
 # Variable declarations
+purchase = 0.0
+stateTax = 0.0
+countyTax = 0.0
+totalTax = 0.0
+totalSale = 0.0
 
 # Constants for the state and county tax rates
+STATE_TAX_RATE = 0.05
+COUNTY_TAX_RATE = 0.025
 
 # Get the amount of the purchase.
+purchase = float(input("Enter the amount of the purchase: "))
 
 # Calculate the state sales tax.
+stateTax = purchase * STATE_TAX_RATE 
 
 # Calculate the county sales tax.
+countyTax = purchase * COUNTY_TAX_RATE 
 
 # Calculate the total tax.
+totalTax = stateTax + countyTax 
 
 # Calculate the total of the sale.
+totalSale = purchase + totalTax
 
 # Print information about the sale.
+print("Purchase Amount;", format(purchase, '.2f'))
+print("State Tax;", format(stateTax, '.2f'))
+print("County Tax;", format(countyTax, '.2f')) 
+print("Total Tax;", format(totalTax, '.2f'))
+print ("Sale Total;", format(totalSale, '.2f'))
