269c2d4f302408590d27b48f9f387e8c60171647
Jay <jaykahoun@westernwyoming.edu>
2025-03-04 03:40:12 +0000

    Ready for Grading

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -1,7 +1,32 @@
 #
-# Name
-# Date
+# Jay
+# 03/03/2025
 # Budget Analysis Programming Project
 # COSC 1010
-#
-# Use comments liberally throughout the program. 
+
+# Variables 
+userBudget = 0
+moreExpenses = 0
+userTotalExpenses = 0
+userExpenses = 0
+totalExpenses = 0
+
+userBudget = float( input( "Please enter how much you've budgeted " + \
+                           "for the month: " ))
+moreExpenses = "y"
+userTotalExpenses = 0
+
+while moreExpenses == "y":
+    userExpenses = float(input("Enter an expense: "))
+    userTotalExpenses += userExpenses
+    moreExpenses = input("Do you have more expenses?: Type y "+ \
+                         "For yes, any key for no: " )
+if userTotalExpenses > userBudget: 
+    print("You were over your budget of","$", format(userBudget, ",.2f"), \
+          "by $",format(userTotalExpenses - userBudget, ",.2f"))
+elif userBudget > totalExpenses: 
+    print("You were under your budget of","$" + format (userBudget, ",.2f"), \
+            "by $", format(userBudget - userTotalExpenses, ",.2f"))
+else: 
+    print("You uses exactly your budget of","$", \
+          format(userBudget, ",.2f"), ".")
