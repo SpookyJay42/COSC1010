e5cc8effaf61ce0c69f2066ff4d440eae1d0895c
Jay <jaykahoun@westernwyoming.edu>
2025-04-07 23:06:21 +0000

    Ready for Grading

--- Average-of-Numbers/main.py
+++ Average-of-Numbers/main.py
@@ -1,7 +1,19 @@
 #
-# Name
-# Date
+# Jay 
+# 4/5/25
 # Average of Numbers Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+def main():
+    numbersFile = open ("numbers.txt", "r")
+
+    total = 0 
+    numbersOfLines = 0
+    line = numbersFile.readline()
+
+    average = total / numbersOfLines
+
+    print("The average is", average)
+
+main()
