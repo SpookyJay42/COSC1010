898fbc8ec2ff49ec797d885bc97ebb865a4497dc
Jay <jaykahoun@westernwyoming.edu>
2025-04-08 00:05:35 +0000

    Ready for Grading..

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,17 @@
 #
-# Name
-# Date
+# Jay
+# 4/5/25
 # File Display Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+def main():
+    numbersFile = open("numbers.txt", "r")
+
+    line = numbersFile.readline()
+
+    while line != "":
+        print(line.rstrip("\n"))
+        line = numbersFile.readline()
+
+main()
