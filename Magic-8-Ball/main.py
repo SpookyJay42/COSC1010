dd40a2daadc67fc987531c751dc745348c86a0a8
Jay <jaykahoun@westernwyoming.edu>
2025-04-14 04:19:13 +0000

    Ready for grading>?>

--- Magic-8-Ball/main.py
+++ Magic-8-Ball/main.py
@@ -4,4 +4,19 @@
 # Magic 8 Ball Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# 
+import random
+
+answers = [
+    "Yes",
+    "No",
+    "Maybe",
+    "Ask again later",
+    "Definitely",
+    "Absolutely not",]
+
+print("Your lucky number is", random.randint(1, 100))
+
+while True:
+    x = input("\nAsk a yes or no question and press enter > ")
+    print(random.choice(answers))
