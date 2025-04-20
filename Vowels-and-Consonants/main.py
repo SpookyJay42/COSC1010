fd6870a89ed661b06ea4f0de9a4d66f79d27b783
Jay <jaykahoun@westernwyoming.edu>
2025-04-20 23:48:30 +0000

    Ready for grading>

--- Vowels-and-Consonants/main.py
+++ Vowels-and-Consonants/main.py
@@ -1,7 +1,30 @@
 #
-# Name
-# Date
+# Jay
+# 4/19/2025
 # Vowels and Consonants Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+
+vowels ="aeiouAEIUO"
+def count_vowels(input_string):
+    vowel_count = 0
+    for char in input_string:
+        if char in vowels:
+            count_vowel +=1
+    return vowel_count
+def count_consonants(input_string):  
+    consonant_count = 0
+    for char in input_string:
+        if char.isalpha() and char not in vowels:
+            consonant_count += 1
+    return consonant_count
+
+user_input = input("Enter a string: ")
+vowel_count = count_vowels(user_input)
+consonant_count = count_consonants(user_input)
+
+print("Number of vowels in string are: ", vowel_count)
+print("Number of consonants in the sting are ",consonant_count)
+
+
+
